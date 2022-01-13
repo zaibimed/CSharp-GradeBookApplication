8f31c9b800322f09996e7b47bb428256add933d0
ZaibiMed <mohammed.zaibi@outlook.com>
2022-01-13 12:31:27 +0100

    init 1

---
+++ GradeBook/Enums/GradeBookType.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GradeBook.Enums
+{
+  public enum GradeBookEnum
+  {
+    Standard,
+    Ranked,
+    ESNU,
+    OneToFour,
+    SixPoint
+  }
+}
