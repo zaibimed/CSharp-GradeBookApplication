473b8115f12bfcbdc885fcae5b4a8eb066ed9cab
ZaibiMed <mohammed.zaibi@outlook.com>
2022-01-14 11:08:01 +0100

    module 3

--- GradeBook/GradeBooks/RankedGradeBook.cs
+++ GradeBook/GradeBooks/RankedGradeBook.cs
@@ -10,5 +10,34 @@ namespace GradeBook.GradeBooks
     {
       Type = Enums.GradeBookType.Ranked;
     }
+
+    public override char GetLetterGrade(double averageGrade)
+    {
+      if (averageGrade >= 80)
+      {
+        throw new InvalidOperationException();
+      }
+
+      if(averageGrade >= 0 && averageGrade < 20)
+      {
+        return 'A';
+      }
+
+      if(averageGrade >= 20 && averageGrade < 40)
+      {
+        return 'B';
+      }
+
+      if (averageGrade >= 40 && averageGrade < 60)
+      {
+        return 'C';
+      }
+
+      if (averageGrade >= 60 && averageGrade < 80)
+      {
+        return 'D';
+      }
+      return 'F';
+    }
   }
 }
